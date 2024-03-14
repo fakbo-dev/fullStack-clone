bf9083cc11233d30d5d8856ef7ef6830103d4db0
fakbo <fakbo0206@gmail.com>
2024-03-14 19:36:16 -0400

    yeah

---
+++ huXn-webDev/advance-javaScript/primitive-Data/string/main/main.js
@@ -0,0 +1,39 @@
+// String
+
+let firstName = "Fakbo ";
+let secondName = "Oropeza";
+
+//1. Concatenation
+
+const fullName = firstName.concat(secondName);
+
+//2. apend
+
+firstName += " something else";
+
+//3. Length
+
+firstName.length;
+
+//4. cases 
+
+firstName.toUpperCase();
+firstName.toLowerCase();
+
+//5 Slice
+
+firstName.slice(0, 3);
+
+//6 Split & join 
+
+console.log(firstName.split(""));
+
+//7. includes
+
+console.log(firstName.toLowerCase().includes("fak"));
+console.log(firstName);
+
+
+//8. trim
+
+console.log(firstName.trim());
