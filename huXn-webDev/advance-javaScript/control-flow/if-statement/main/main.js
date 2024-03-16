efbf979813ecabf3f95d1b7d58e583237ceba51e
fakbo <fakbo0206@gmail.com>
2024-03-16 10:39:50 -0400

    easy if

---
+++ huXn-webDev/advance-javaScript/control-flow/if-statement/main/main.js
@@ -0,0 +1,23 @@
+// Conditional Statement
+
+// if === money
+// -> by Some stuff
+// else !== money
+// -> bring some money
+
+
+// if (condition) {...}
+// else if (condition) {...}
+// else {...}
+
+let a = 10;
+let b = 20;
+
+if (a > b) {
+    console.log("A is greater then b");
+}
+else if (a < b) {
+    console.log("A is less then b");
+} else {
+    console.log("A is equal to B")
+}
---
+++ huXn-webDev/advance-javaScript/primitive-Data/type-conversion/main/main.js
@@ -0,0 +1,29 @@
+// Type Conversion
+
+let money = "50";
+
+// !Convert string to number
+
+//? money = parseInt(money);
+//? money = +money;
+//? money = Number(money);
+
+
+// !Convert number to string
+
+let moneytwo = 50;
+
+// money = money.toString();
+// money = String(money);
+
+// !Convert string to decimal (float)
+
+let floatNumber = "50.245";
+
+floatNumber = parseFloat(money);
+
+console.log(money);
+console.log(typeof money);
+
+console.log(floatNumber);
+console.lof(typeof floatNumber)
