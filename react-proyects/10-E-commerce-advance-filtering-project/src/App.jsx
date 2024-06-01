9b8697b52711b0958fdcdf651d3a8fcb2585cec8
fakbo <fakbo0206@gmail.com>
2024-05-31 22:25:54 -0400

    setup done

---
+++ react-proyects/10-E-commerce-advance-filtering-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Navigation from "./Navigation/Nav.jsx";
+import Products from "./Products/Products.jsx";
+import Recommended from "./Recommended/Recommended.jsx";
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Products />
+      <Recommended />
+    </>
+  )
+}
+
+export default App
---
+++ react-proyects/10-E-commerce-advance-filtering-project/src/Sidebar/Category/Category.jsx
@@ -0,0 +1,8 @@
+import "./Category/Category.css"
+function Category() {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
