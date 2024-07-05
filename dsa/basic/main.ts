b1ee5eb98031916e7f40ddb0775892031bb1847d
fakbo <fakbo0206@gmail.com>
2024-07-05 12:40:32 -0400

    starting big O

---
+++ dsa/basic/main.ts
@@ -0,0 +1,32 @@
+// Data structure: a data structure is a specific way of organizing, storing and accessing data.
+// Algorithm: a set of instructions that tells a computer how to do something, or you can also say step-by-step solutions of the problem is called algorithms
+
+// Problem 1
+
+// Create an Array with 5 students names, after that create a function takes 2 parameters (allStudents, studentName) iterate over all students and find that specific user "studentName";
+
+
+//Step 1 create an array of 5 students
+const studentsName: string[] = ["Virgie Graham", "Catherine Fox", "Bessie Brewer", "Timothy Bell", "Hattie Griffin"];
+
+
+//Step 2 create a function takes 2 parameters
+
+function findStudents(allStudents: string[], studentName: string): void {
+
+    // step 3 iterate over all student
+
+    for (let i = 0; i < allStudents.length; i++) {
+        // step 4 find specific user "studentName"
+        let index = allStudents[i];
+
+        if (index === studentName) {
+            console.log(`Find Student:${index}`)
+            break;
+        }
+        console.log(`Student Not found`);
+    }
+}
+
+
+findStudents(studentsName, "Hattie Griffin");
