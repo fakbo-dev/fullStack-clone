7c55bf2f0cac25527761f9f1f20c646379b09c78
fakbo <fakbo0206@gmail.com>
2024-07-09 19:56:16 -0400

    maxProfit

---
+++ dsa/03-ds-array/tasks/06-maxprofit/main.ts
@@ -0,0 +1,11 @@
+// MaxProfit
+
+// Imagine you're buying and selling stocks throughout the year. your job is to find the biggest profit you could make by buying low and selling high only once
+
+// here's what you're given
+
+// A list of stock prices for each day -> [7,1,5,3,6,4]
+
+// Here's what you need to find:
+
+// The difference between the cheapest price and could have bought the stock and the most expensive price you could have sold it later on
