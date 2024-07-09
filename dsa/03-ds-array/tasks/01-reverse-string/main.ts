2e9a20e2fe7d2e955e5a93e183a353b8f7a53fe9
fakbo <fakbo0206@gmail.com>
2024-07-09 11:29:31 -0400

    start with palindrome

---
+++ dsa/03-ds-array/tasks/01-reverse-string/main.ts
@@ -0,0 +1,30 @@
+// Take a string a reverse each letter
+
+// example
+// Hello = olleH
+// Apple = elppA;
+
+
+
+
+const reverseString = (str: string): string => {
+
+    // 1. transformar el string en un array
+    let stringArray = str.toLowerCase().split("");
+
+    let reversedString: string = "";
+    // 2. como puedo dar vuelta al string? => iterar a travez de mi array empezando desde el ultimo valor hasta el valor en el primer indice "0" decrementando su iteracion 
+
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reversedString += stringArray[i];
+    }
+    //3. Tomar el valor final y volver su primer valor a mayuscula 
+    const result = reversedString.charAt(0).toUpperCase() + reversedString.slice(1);
+    return result;
+
+}
+
+
+
+console.log(reverseString("Hello")); //Expected ouput: "Olleh";
+console.log(reverseString("Apple")); //Expected ouput: "Elppa"
---
+++ dsa/03-ds-array/tasks/02-palindrome/main.ts
@@ -0,0 +1,8 @@
+// Palindromes
+
+// if the reverse string is equal to the original one then that word is a palindrome
+
+// cddc === cddc ✅
+// abba === abba ✅
+
+// Hello !== olleH ❌
