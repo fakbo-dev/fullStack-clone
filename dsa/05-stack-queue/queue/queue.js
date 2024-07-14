ff6dbf0a03b4b7ceb5d58b3a975c8def1e09aea0
fakbo <fakbo0206@gmail.com>
2024-07-14 17:05:45 -0400

    starting queue

---
+++ dsa/05-stack-queue/queue/queue.js
@@ -0,0 +1,4 @@
+// Queue
+
+// A queue is a linear data structure that functions like a wating line. it follows the FIFO (First in, First Out) principe, meaning the element that enters the queue first will be first one to be removed.
+
