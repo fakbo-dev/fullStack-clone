8f623f521ada30cdf6b67169891b9ea24b7d1a76
fakbo <fakbo0206@gmail.com>
2024-07-16 09:44:06 -0400

    is a startMan

--- dsa/07-tree/basic/treeBasic.js
+++ dsa/07-tree/basic/treeBasic.js
@@ -1,4 +1,49 @@
 // Tree
 
 // A tree is a fundamental DS (Data Structure) hierarchical data structure. it represents a collection of elements (data) called nodes, connected by edges (links)
-// forming a tree-like structure
+// forming a tree-like structure ----> check the excalidraw for more information
+
+// BST (Binary Search Tree)
+
+
+// a BST is a special kind of binary tree used to organize data in a sorted way. it works like a filing cabinet where you can efficiently search, add, or remove items
+
+
+
+class Node {
+
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BST {
+
+    constructor() {
+        this.root = null;
+    }
+
+    // #1 insert add a node to the BST and check in each leaf is the value is greater or less than the root and the next node, if is less go to the left if is greater go to the right
+    insert(data) {
+        const newNode = new Node(data);
+
+        if (this.root === null) this.root = newNode;
+
+        
+    }
+}
+
+
+const tree = new BST();
+
+console.log(tree);
+tree.insert(5);
+// tree.insert(8);
+// tree.insert(3);
+// tree.insert(1);
+// tree.insert(7);
+// tree.insert(9);
+console.log(tree);
