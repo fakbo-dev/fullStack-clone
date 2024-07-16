ebcb3db99a502dee056401a52e9a742cfab0345f
fakbo <fakbo0206@gmail.com>
2024-07-15 20:53:25 -0400

    leaf

---
+++ dsa/07-tree/basic/treeBasic.js
@@ -0,0 +1,4 @@
+// Tree
+
+// A tree is a fundamental DS (Data Structure) hierarchical data structure. it represents a collection of elements (data) called nodes, connected by edges (links)
+// forming a tree-like structure
