1e7159af4275ba2dfb999397a2a878e6e79d581a
fakbo <fakbo0206@gmail.com>
2024-07-29 15:39:52 -0400

    i'm gonna finish later on

--- dsa/06-hashtable/basic/main.js
+++ dsa/06-hashtable/basic/main.js
@@ -26,17 +26,22 @@
 // Fixed-Size Ouput -> Regardless of the input size, the hash function always spits out a fixed-size value, like  a short string of characters or a number.
 
 
+
 class HashTable {
-    constructor(size = 6) {
+
+    constructor(size = 5) {
         this.keyMap = new Array(size);
+
     }
 
-    _hashFunction(key) {
+    hashFunc(key) {
         let sum = 0;
+
         const PRIME_NUMBER = 31;
 
         for (let i = 0; i < Math.min(key.length, 100); i++) {
             const charCode = key.charCodeAt(0) - 96;
+
             sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length;
         }
 
@@ -44,54 +49,13 @@ class HashTable {
     }
 
     set(key, value) {
-        const index = this._hashFunction(key);
+
+        const index = this.hashFunc(key);
         if (!this.keyMap[index]) this.keyMap[index] = [];
         this.keyMap[index].push([key, value]);
         return this;
     }
-
-    get(key) {
-        const index = this._hashFunction(key);
-        if (this.keyMap[index]) {
-            for (let i = 0; i < this.keyMap[index].length; i++) {
-                if (this.keyMap[index][i][0] === key) {
-                    return this.keyMap[index][i][1];
-                }
-            }
-        }
-        return undefined;
-    }
-
-    getAllKeys() {
-        const keys = [];
-        for (let i = 0; i < this.keyMap.length; i++) {
-            if (this.keyMap[i]) {
-                for (let j = 0; j < this.keyMap[i].length; j++) {
-                    keys.push(this.keyMap[i][j][0]);
-                }
-            }
-        }
-        return keys;
-    }
-
-    getAllValues() {
-        const values = [];
-        for (let i = 0; i < this.keyMap.length; i++) {
-            if (this.keyMap[i]) {
-                for (let j = 0; j < this.keyMap[i].length; j++) {
-                    values.push(this.keyMap[i][j][1]);
-                }
-            }
-        }
-        return values;
-    }
 }
 
-const phoneBook = new HashTable("john", "555-333-222");
-phoneBook.set("john", "555-333-444");
-phoneBook.set("jordan", "222-444-666");
-phoneBook.set("michel", "111-666-222");
-console.log(phoneBook.get("jordan"));
-console.log(phoneBook.getAllValues());
-console.log(phoneBook.getAllKeys());
-phoneBook.set();
+const phoneBook = new HashTable();
+console.log(phoneBook.set("john", "555-333-444"));
