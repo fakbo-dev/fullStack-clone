55d824b1273055a3568179afbc558d8c4eff1b4c
fakbo <fakbo0206@gmail.com>
2024-08-09 17:58:02 -0400

    start

--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -15,7 +15,6 @@ const Home = () => {
       <button className="border px-2 py-4" onClick={() => navigate("about")}>Go To About Page</button>
 
       <button className="border px-2 py-4 block my-3" onClick={() => navigate("login")}>Go To About Login</button>
-
     </section>
   )
 }
